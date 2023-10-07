b0411b530f9a3355f647e0b155edfd75d99bcd30
Banggi Bima <angetaiayam@gmail.com>
2023-10-08 00:57:06 +0700

    Revert "merge pull request #2 from banggibima/feature/add-argon2-hash"
    This reverts commit b39b022f05df9d65e020a614c80a631066684e52, reversing
    changes made to 4bf4cfcb9ff432b60187cda40b2495a87eef00c3.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,10 @@
 import express from "express";
 import cors from "cors";
-import permission from "./modules/permission/interface/router";
-import profile from "./modules/profile/interface/router";
-import role from "./modules/role/interface/router";
-import user from "./modules/user/interface/router";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/permissions", permission);
-app.use("/api/profiles", profile);
-app.use("/api/roles", role);
-app.use("/api/users", user);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import app from "./app";
 import env from "./config/env";
 
-app.listen(env.port, () => {
-  console.log({
-    message: `server running at http://${env.host}:${env.port}`,
-  });
-});
+app.listen(env.port);
